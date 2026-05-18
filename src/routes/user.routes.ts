2b90a9fefaa452c0cf2514a3f06d3d39b92f06c1
YashChoudam <yashchoudam527@gmail.com>
2026-05-18 19:56:22 +0530

    Package updates

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -19,7 +19,7 @@ userRoutes.use(express.json());
 //Signup
 userRoutes.post("/signup", async (req, res) => {
   const { email, username, password } = req.body;
-
+  
   try {
     const salt = await bcrypt.genSalt(12);
     const hashedPassword = await bcrypt.hash(password, salt);
@@ -39,150 +39,150 @@ userRoutes.post("/signup", async (req, res) => {
   }
 });
 
-// Login
+
+// Login 
 userRoutes.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  const userJwtSecret = process.env.JWT_USERSECRET;
+    const { email, password } = req.body;
+    const userJwtSecret = process.env.JWT_USERSECRET;
 
-  if (!userJwtSecret) {
-    throw new Error("JWT_USERSECRET is missing");
-  }
+    if (!userJwtSecret) {
+        throw new Error("JWT_USERSECRET is missing");
+    }
 
-  const response = await userModel.findOne({
-    email,
-  });
-
-  if (!response) {
-    return res.status(404).json({
-      message: "User does not exist with this email",
+    const response = await userModel.findOne({
+        email
     });
-  }
-  const passwordMatch = await bcrypt.compare(password, response.password);
-  if (passwordMatch) {
-    const token = jwt.sign(
-      {
-        id: response._id,
-        email: response.email,
-      },
-      userJwtSecret,
-    );
-    return res.json({ token });
-  } else {
-    return res.status(401).json({ message: "Incorrect Credentials" });
-  }
+
+    if (!response) {
+        return res.status(404).json({
+            message: "User does not exist with this email"
+        });
+    }
+    const passwordMatch = await bcrypt.compare(password,response.password);
+    if (passwordMatch) {
+        const token = jwt.sign(
+            {
+                id: response._id,
+                email: response.email,
+            },
+            userJwtSecret
+        );
+        return res.json({ token });
+    } else {
+        return res.status(401).json({ message: "Incorrect Credentials"});
+    }
 });
 
-userRoutes.post("/content", authUser, async (req, res) => {
-  const { link, type, title, tags } = req.body;
+userRoutes.post("/content", authUser , async (req,res)=>{
+    const {link , type , title , tags} = req.body ;
 
-  try {
-    if (!req.user) {
-      return res.status(401).json({
-        message: "Unauthorized user",
-      });
+    try {
+        if (!req.user){
+            return res.status(401).json({
+                message : "Unauthorized user",
+            });
+        }
+        const content = await contentModel.create({
+            link : link ,
+            type : type ,
+            title : title ,
+            tags : tags ,
+            userId : req.user.id,
+        })
+        return res.status(201).json({
+            message : "Content added successfully",
+            content
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message : "Error while adding content"
+        })
     }
-    const content = await contentModel.create({
-      link: link,
-      type: type,
-      title: title,
-      tags: tags,
-      userId: req.user.id,
-    });
-    return res.status(201).json({
-      message: "Content added successfully",
-      content,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error while adding content",
-    });
-  }
+})
+
+userRoutes.get("/content" , authUser , async (req,res)=>{
+    try {
+        if(!req.user){
+            return res.status(401).json({
+                message : "Unauthorized User",
+            });
+        }
+        const content = await contentModel.find({
+            userId : req.user.id ,
+        })
+        .populate("tags");
+        return res.status(200).json({
+            message : "Content fetched Successfully",
+            content
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message : "Error fetching content"
+        });
+    }
 });
 
-userRoutes.get("/content", authUser, async (req, res) => {
-  try {
-    if (!req.user) {
-      return res.status(401).json({
-        message: "Unauthorized User",
-      });
+userRoutes.delete("/content/:contentId" , authUser , async (req,res)=>{
+    const {contentId} = req.params ;
+    try {
+        if (!req.user) {
+            return res.status(401).json({
+                message : "Unauthorized access" 
+            });
+        }
+        const deletedContent = await contentModel.findOneAndDelete({
+            _id: contentId,
+            userId: req.user.id,
+        });
+        if (!deletedContent) {
+            return res.status(404).json({
+                message : "Content not found or you are not allowed to delete it"
+            });
+        }
+        return res.status(200).json({
+            message : "Content deleted successfully",
+            deletedContent,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message : "Error while deleting content"
+        });
     }
-    const content = await contentModel
-      .find({
-        userId: req.user.id,
-      })
-      .populate("tags");
-    return res.status(200).json({
-      message: "Content fetched Successfully",
-      content,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error fetching content",
-    });
-  }
 });
 
-userRoutes.delete("/content/:contentId", authUser, async (req, res) => {
-  const { contentId } = req.params;
-  try {
-    if (!req.user) {
-      return res.status(401).json({
-        message: "Unauthorized access",
-      });
-    }
-    const deletedContent = await contentModel.findOneAndDelete({
-      _id: contentId,
-      userId: req.user.id,
-    });
-    if (!deletedContent) {
-      return res.status(404).json({
-        message: "Content not found or you are not allowed to delete it",
-      });
-    }
-    return res.status(200).json({
-      message: "Content deleted successfully",
-      deletedContent,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error while deleting content",
-    });
-  }
-});
 
-userRoutes.post("/brain/share", authUser, async (req, res) => {
-  try {
-    if (!req.user) {
-      return res.status(401).json({
-        message: "Unauthorized user",
-      });
-    }
+userRoutes.post("/brain/share" , authUser , async (req,res)=>{
+    try {
+        if (!req.user) {
+            return res.status(401).json({
+                message : "Unauthorized user"
+            });
+        }
+        const existingLink = await linkModel.findOne({
+            userId: req.user.id,
+        });
+        if (existingLink) {
+            return res.status(200).json({
+                message : "Share link already exists ",
+                hash : existingLink.hash,
+                shareUrl : `/api/v1/user/brain/${existingLink.hash}`,
+            });
+        }
+        const hash = crypto.randomBytes(16).toString("hex");
 
-    const existingLink = await linkModel.findOne({
-      userId: req.user.id,
-    });
-    if (existingLink) {
-      return res.status(200).json({
-        message: "Share link already exists ",
-        hash: existingLink.hash,
-        shareUrl: `/api/v1/user/brain/${existingLink.hash}`,
-      });
-    }
-    const hash = crypto.randomBytes(16).toString("hex");
-
-    const link = await linkModel.create({
-      hash,
-      userId: req.user.id,
-    });
-    return res.status(201).json({
-      message: "Share link created successfully",
-      hash: link.hash,
-      shareUrl: `/api/v1/user/brain/${link.hash}`,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error while creating sharable link",
-    });
-  }
-});
-export { userRoutes };
+        const link = await linkModel.create({
+            hash,
+            userId : req.user.id,
+        })
+        return res.status(201).json({
+            message : "Share link created successfully",
+            hash : link.hash ,
+            shareUrl : `/api/v1/user/brain/${link.hash}`,
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message : "Error while creating sharable link"
+        });
+    };
+})
+export {userRoutes} ;
